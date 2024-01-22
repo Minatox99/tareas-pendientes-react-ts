91934e4ac120fee8675c918739feab8324dae540
Minato <erms12.99@hotmail.com>
2024-01-21 22:56:17 -0600

    Proyecto final listado de tareas con React y TS

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { createRoot } from 'react-dom'; 
+import ReactDOM from 'react-dom';
 import './styles.css';
 import { TodoApp } from './components/TodoApp';
 
-createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <TodoApp />
   </React.StrictMode>,
+  document.getElementById('root')
 );
